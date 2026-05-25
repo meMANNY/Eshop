85849693f46f2cc321ee95454a7eebeff9786770
Aman Tripathi <amanttripathi02@gmail.com>
2026-05-25 21:25:53 +0530

    added cookie-parser auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
 const host = process.env.HOST ?? 'localhost';
 // const port = process.env.PORT ? Number(process.env.PORT) : 6001; //3000 is my nextjs project port.
 
@@ -11,6 +12,8 @@ app.use(cors({
     credentials: true //if credentials true then we cannot use origin "*"
 }));
 
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API' });
