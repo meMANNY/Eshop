4a176c5b166e488c14499a953146545f9fc6beb2
Aman Tripathi <amanttripathi02@gmail.com>
2026-05-25 14:52:14 +0530

    setup the api-gateway server

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,15 +4,52 @@
  */
 
 import express from 'express';
-import * as path from 'path';
+//import * as path from 'path';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from "morgan";
+import rateLimit from 'express-rate-limit';
+import swaggerUi from "swagger-ui-express";
+import cookieParser from 'cookie-parser';
+import axios from 'axios';
+
+
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ['Authorization', 'Content-Type',],
+  credentials: true //if credentials true then we cannot use origin "*"
+}));
 
-app.get('/api', (req, res) => {
+app.use(morgan('dev'));
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+
+//rate limit logic
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: "Too many requests",
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req: any) => req.ip
+});
+
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
+
+app.use("/", proxy("http://localhost:6001"));
+
+
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
