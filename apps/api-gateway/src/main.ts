fdd81acdfbfa4e13ebb30f1bec364cd35a2446da
Aman Tripathi <amanttripathi02@gmail.com>
2026-06-27 20:25:59 +0530

    modified import error

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -9,9 +9,9 @@ import cors from 'cors';
 import proxy from 'express-http-proxy';
 import morgan from "morgan";
 import rateLimit from 'express-rate-limit';
-import swaggerUi from "swagger-ui-express";
+//import swaggerUi from "swagger-ui-express";
 import cookieParser from 'cookie-parser';
-import axios from 'axios';
+//import axios from 'axios';
 
 
 
---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,14 @@
+
+
+
+
+// Register a new user 
+
+import { NextFunction } from "express";
+
+export const userRegistration = async(req: Request, res: Response, next: NextFunction) =>{
+
+    
+
+    
+}
