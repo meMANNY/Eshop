8f4b4a14a71edb233bb766d257de912cfb2610f8
Aman Tripathi <amanttripathi02@gmail.com>
2026-07-01 13:42:45 +0530

    Fixed Swagger doc issue, as well as nx build issue

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -3,7 +3,15 @@ import cors from 'cors';
 import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
 import cookieParser from 'cookie-parser';
 import router from './routes/auth.router';
+import swaggerUi from 'swagger-ui-express';
+
+
+
+const swaggerDocument = require('./swagger-output.json');
 const host = process.env.HOST ?? 'localhost';
+
+
+
 // const port = process.env.PORT ? Number(process.env.PORT) : 6001; //3000 is my nextjs project port.
 
 const app = express();
@@ -19,13 +27,17 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API' });
 });
-
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/docs-json", (req, res) => {
+    res.json(swaggerDocument);
+});
 app.use("/api",router);
 
 app.use(errorMiddleware)
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
     console.log(`[ ready ] http://${host}:${port}/api`);
+    console.log(`Swagger Docs available at http://${host}:${port}/api-docs`);
 });
 
 
