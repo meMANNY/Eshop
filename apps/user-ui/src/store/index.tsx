4738a5643fe78773451b62230828a44756bd79d1
Aman Tripathi <amanttripathi02@gmail.com>
2026-08-09 04:44:36 +0530

    created zustand store for cart and wishlist

--- apps/user-ui/src/store/index.tsx
+++ apps/user-ui/src/store/index.tsx
@@ -11,7 +11,7 @@ type Product = {
 };
 
 type Store = {
-    card: Product[];
+    cart: Product[];
     wishlist: Product[];
 
     addToCart: (
@@ -42,3 +42,77 @@ type Store = {
         deviceInfo: any
     ) => Promise<void> | void;
 };
+
+export const useStore = create<Store>()(
+    persist(
+        (set,get) =>({
+            cart: [],
+            wishlist: [],
+
+            addToCart: async(
+                product,
+                user,
+                location,
+                deviceInfo
+
+            )=>{
+                set((state)=>{
+                    const existing  = state.cart.find((i)=>i.id === product.id);
+                    if(existing){
+                        return {
+                            cart: state.cart.map((i)=>i.id === product.id ? {...i, quantity: (i.quantity ?? 1) + 1} : i)
+                        }
+                    }
+                    return {
+                        cart: [...state.cart, {...product, quantity: product?.quantity}]
+                    }
+                });
+
+
+            },
+            removeFromCart: async(id,user,location,deviceInfo) => {
+
+                const removeProduct = get().cart.find((i)=>i.id === id);
+
+                set((state) => ({
+                    cart: state.cart.filter((i) => i.id !== id)
+                }));
+                if (!removeProduct) {
+                    console.warn("CLIENT SKIP track remove_from_cart (no product)", {
+                        productId: id,
+                    });
+                    return;
+                }
+            },
+            addToWishlist: async(product,user,location,deviceInfo) => {
+                set((state) => {
+                    const existing = state.wishlist.find((i) => i.id === product.id);
+                    if (existing) {
+                        return state; // Product already in wishlist, do nothing
+                    }
+                    return {
+                        wishlist: [...state.wishlist, product],
+                    };
+                });
+            },
+
+            removeFromWishlist: async(id,user,location,deviceInfo) => {
+                const removeProduct = get().wishlist.find((i) => i.id === id);
+                 set((state) => ({
+                    wishlist: state.wishlist.filter((i) => i.id !== id),
+                    }));
+
+                    if (!removeProduct) {
+                    console.warn("CLIENT SKIP track remove_from_wishlist (no product)", {
+                        productId: id,
+                    });
+                    return;
+                }
+            },
+
+        }),
+
+        {name: "ecommerce-store"}
+        
+    )
+);
