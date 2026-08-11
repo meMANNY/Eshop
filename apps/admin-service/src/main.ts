94399a45646fa70feebae779caa8ae320812679d
Aman Tripathi <amanttripathi02@gmail.com>
2026-08-12 02:51:53 +0530

    initialized admin ui

--- apps/admin-service/src/main.ts
+++ apps/admin-service/src/main.ts
@@ -1,21 +1,47 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import express from 'express';
-import * as path from 'path';
+import router from "./routes/adminRoutes";
+import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+    ],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  })
+);
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to admin-service!' });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+  // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // app.get("/docs-json", (req, res) => {
+  //   res.json(swaggerDocument);
+  // });
+
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to admin-service!" });
 });
 
-const port = process.env.PORT || 3333;
+app.use("/api", router);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6005;
+
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Admin service listening at http://localhost:${port}/api`);
+  // console.log(`Swagger Docs available at http://localhost:${port}/api-docs`);
 });
-server.on('error', console.error);
+server.on("error", (err) => {
+  console.log("Server Error :", err);
+});
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -47,6 +47,7 @@ app.use(limiter);
 app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
+app.use("/admin", proxy("http://localhost:6005"));
 app.use("/order", proxy("http://localhost:6004"));
 app.use("/product",proxy("http://localhost:6002"));
 
