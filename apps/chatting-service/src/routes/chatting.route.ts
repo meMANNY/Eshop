191dbf6fc1350e8623b6b309b1ac093a9b4a7f2a
Aman Tripathi <amanttripathi02@gmail.com>
2026-08-15 04:34:29 +0530

    created routes for chatting service

--- apps/chatting-service/src/routes/chatting.route.ts
+++ apps/chatting-service/src/routes/chatting.route.ts
@@ -1,5 +1,32 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import { isAuthenticated } from "../../../../packages/middleware/isAuthenticated"
+import { isSeller } from "../../../../packages/middleware/isSeller"
+import { 
+  fetchSellerMessages,
+  fetchUserMessages,
+  getSellerConversations,
+  getUserConversations,
+  newConversation,
+} from "../controller/chatting.controller";
 
-const router = Router();
+const router: Router = express.Router();
+
+router.post("/create-user-conversationGroup", isAuthenticated, newConversation);
+router.get("/get-user-conversations", isAuthenticated, getUserConversations);
+router.get(
+  "/get-seller-conversations",
+  isSeller,
+  getSellerConversations
+);
+router.get(
+  "/get-user-messages/:conversationId",
+  isAuthenticated,
+  fetchUserMessages
+);
+router.get(
+  "/get-seller-messages/:conversationId",
+  isSeller,
+  fetchSellerMessages
+);
 
 export default router;
